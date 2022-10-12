cc32729984448272a8eeda0f11dbf109c0a455cc
Liuba <kundasl1@gmail.com>
2022-10-12 17:36:25 +0300

    create cards

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,22 @@
 from django.views.generic import TemplateView
+from datetime import datetime
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+    # Get all previous data
+        context = super().get_context_data(**kwargs)
+
+        # Create your own data
+        context["news_title"] = "Новость"
+        context["news_preview"] = "Предварительное описание новости"
+        context["news_date"] = datetime.now()
+        context["range"] = range(5)
+        return context
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
