69e101a7d11f6b492b3abcd355cdeefd2bd66e09
Liuba <kundasl1@gmail.com>
2022-10-13 00:08:12 +0300

    add filtr email

--- mainapp/templates/templatetags/email_to_link.py
+++ mainapp/templates/templatetags/email_to_link.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter
 def email_to_link(value):
-    return mark_safe(f"<a href='mailto:{value}'>{value}<a/> ")
+    return mark_safe(f"<a href='mailto:{value}'>{value}<a/>")
