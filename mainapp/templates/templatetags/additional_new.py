7fdb3f327c335465fc5d9af20beef295ed69b64a
Liuba <kundasl1@gmail.com>
2022-10-13 00:08:12 +0300

    add filtr email

--- mainapp/templates/templatetags/additional_new.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from django.utils.safestring import mark_safe
-
-additional = template.Library()
-
-@additional.filter
-def additional_new(value):
-    return mark_safe(f"<a href='mailto:{value}'>{value}<a/> ")
