89307f43c942f84fb73f40a8dae902e654d488fc
Liuba <kundasl1@gmail.com>
2022-10-13 00:36:30 +0300

    add parameter of link new

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+
 from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name # создаем вместе с namespace
@@ -8,6 +9,7 @@ app_name = MainappConfig.name # создаем вместе с namespace
 urlpatterns = [
     path('', MainPageView.as_view(), name='main'),
     path('news/', NewsPageView.as_view(), name='news'),
+    path('news/<int:page>/', NewsWithPaginatorView.as_view(), name='news_paginator'), #add обработка
     path('courses_list/', CoursesPageView.as_view(), name='courses'),
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
     path('doc_site/', DocSitePageView.as_view(), name='docs'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -18,6 +18,13 @@ class NewsPageView(TemplateView):
         context["range"] = range(5)
         return context
 
+class NewsWithPaginatorView(NewsPageView):
+
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
+
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
 
