fd1b4d28ff96676512762df89ed535c942916a8c
Liuba <kundasl1@gmail.com>
2022-10-26 01:34:27 +0300

    user edit form

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,9 +1,23 @@
+import os
+#from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
 class CustomUserCreationForm(UserCreationForm):
-    #можно создать field_order
+    field_order = [
+        "username",
+        "password1",
+        "password2",
+        "email",
+        "first_name",
+        "last_name",
+        "age",
+        "avatar",
+    ]
+
     class Meta:
         model = get_user_model() #лучше использовать get_user_model
         fields = (
@@ -15,3 +29,16 @@ class CustomUserCreationForm(UserCreationForm):
             "avatar",
         )
         field_classes = {"username": UsernameField}
+
+    def clean_avatar(self):
+        arg_as_str = "avatar"
+        if arg_as_str in self.changed_data and self.instance.avatar:
+            if os.path.exists(self.instance.avatar.path):
+                os.remove(self.instance.avatar.path)
+        return self.cleaned_data.get(arg_as_str)
+
+    def clean_age(self):
+        data = self.cleaned_data.get("age")
+        if data < 10 or data > 100:
+            raise ValidationError(_("Please, enter a valid age!"))
+        return data
--- authapp/urls.py
+++ authapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from authapp.views import * #change
+from authapp import views #new
 from authapp.apps import AuthappConfig
 
 app_name = AuthappConfig.name # создаем вместе с namespace
@@ -8,5 +9,8 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile_edit/', ProfileEditView.as_view(), name='profile_edit'),
+    path(
+        "profile_edit/<int:pk>/",
+        views.ProfileEditView.as_view(), name="profile_edit",
+    ), # error403
 ]
